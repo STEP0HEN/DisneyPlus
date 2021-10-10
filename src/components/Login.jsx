f2665465df91ba762cebed9faa876162495fce10
STEP0HEN <guchy17@gmail.com>
2021-10-10 22:25:58 +0200

    fix styling errors

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -43,23 +43,23 @@ const Content = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-directon: column;
+  flex-direction: column;
   padding: 80px 40px;
   height: 100%;
 `;
 
 const BgImage = styled.div`
   height: 100%;
-  width:100%
+  width: 100%;
   background-position: top;
   background-size: cover;
-  background-repeat:no-repeat;
+  background-repeat: no-repeat;
   background-image: url("/images/login-background.jpg");
-  position:absolute;
-  top:0;
-  right:0;
-  left:0;
-  z-index:-1;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
 `;
 
 const Showcase = styled.div`
